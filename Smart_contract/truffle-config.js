266d14df98d222952bd6e0abd99a215aa3458bd0
unknown <okwuosahpaschal@gmail.com>
2022-03-07 12:42:02 +0100

    client commit

--- Smart_contract/truffle-config.js
+++ Smart_contract/truffle-config.js
@@ -3,8 +3,13 @@ const path = require("path");
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "client/contracts"),
+  contracts_build_directory: path.join(__dirname, "../components/contracts"),
   networks: {
+    development: {
+      host: "HTTP://127.0.0.1:",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
     develop: {
       port: 8545
     }
---
+++ pages/index.js
@@ -0,0 +1,146 @@
+import React, { useState, useEffect } from 'react';
+import Head from 'next/head'
+import Image from 'next/image'
+import Web3 from "web3";
+import SimpleStorageContract from "../components/contracts/SimpleStorage.json";
+ 
+const Home = () => {
+  const [state, setState] = useState({
+    storageValue: 0,
+    web3: null,
+    account: null,
+    contract: null,
+  });
+
+  useEffect(() => {
+    const LoadWeb3 = async () => {
+      if (window.ethereum) {
+        try {
+          // Get network provider and web3 instance.
+          const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+
+          const web3 = new Web3(window.ethereum);
+    
+          // Get the contract instance.
+          const networkId = await web3.eth.net.getId();
+          const deployedNetwork = SimpleStorageContract.networks[networkId];
+          const instance = new web3.eth.Contract(
+            SimpleStorageContract.abi,
+            deployedNetwork && deployedNetwork.address,
+          );
+    
+          // Set web3, accounts, and contract to the state, and then proceed with an
+          // example of interacting with the contract's methods.
+          setState({ web3: web3, account: accounts[0], contract: instance, storageValue: 0, });
+        } catch (error) {
+          // Catch any errors for any of the above operations.
+          alert(
+            `Failed to load web3, accounts, or contract. Check console for details.`,
+          );
+          console.error(error);
+        }
+      } else{
+        alert("No web3 instance injected, using Local web3.")
+      }
+    };
+
+    LoadWeb3();
+  }, []);
+
+  const runExample = async () => {
+    const { account, contract } = state;
+    // Stores a given value, 5 by default.
+    try {
+      await contract.methods.set(5).send({ from: account });
+
+      // Get the value from the contract to prove it worked.
+      const response = await contract.methods.get().call();
+
+      // Update state with the result.
+      setState({ storageValue: response });
+    } catch (error) {
+      console.log(error)
+    } 
+  }; 
+
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+        <h1 className="text-6xl font-bold">
+          Welcome to{' '}
+          <a className="text-blue-600" href="https://nextjs.org">
+            Next.js
+          </a>
+        </h1>
+        <p className="mt-3 text-3xl">Count : {state.storageValue}</p>
+        <p className="mt-3 text-2xl">
+          Get started by editing{' '}
+          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
+            pages/index.tsx
+          </code>
+        </p>
+        <button type="button" className="p-3 mt-4 bg-blue-500 text-white" onClick={() => runExample()}>Click Me to update count</button>
+        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
+          <a
+            href="https://nextjs.org/docs"
+            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+          >
+            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
+            <p className="mt-4 text-xl">
+              Find in-depth information about Next.js features and API.
+            </p>
+          </a>
+
+          <a
+            href="https://nextjs.org/learn"
+            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+          >
+            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
+            <p className="mt-4 text-xl">
+              Learn about Next.js in an interactive course with quizzes!
+            </p>
+          </a>
+
+          <a
+            href="https://github.com/vercel/next.js/tree/canary/examples"
+            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+          >
+            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
+            <p className="mt-4 text-xl">
+              Discover and deploy boilerplate example Next.js projects.
+            </p>
+          </a>
+
+          <a
+            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+          >
+            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
+            <p className="mt-4 text-xl">
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </a>
+        </div>
+      </main>
+
+      <footer className="flex h-24 w-full items-center justify-center border-t">
+        <a
+          className="flex items-center justify-center gap-2"
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+        </a>
+      </footer>
+    </div>
+  )
+}
+
+export default Home
